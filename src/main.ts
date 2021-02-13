d6a1d20fa2ebe597506968b5a09762e88200f50b
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-02-13 17:40:57 +0900

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,55 @@
-function hoge(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
-        reject(new Error("hoge"));
-    });
+import * as unified from "unified";
+import * as markdown from "remark-parse";
+import * as is from "unist-util-is";
+import {
+  Break,
+  Content,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  Paragraph,
+  Root,
+  Strong,
+  Text,
+  ThematicBreak,
+} from "mdast";
+
+const processor = unified().use(markdown);
+
+export function parseMarkdown(str: string): string {
+  const root = processor.parse(str);
+  if (is<Root>(root, "root")) {
+    return childrenToString(root.children);
+  }
+  return "";
 }
 
-function fuga(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
-        reject(new Error("fuga"));
-    });
+function childrenToString(children: Content[]): string {
+  return children.reduce((all, current) => all + contentToString(current), "");
 }
 
-async function main() {
-    const a1 = await hoge();
-    const a2 = await fuga();
-    const x = await hoge()
-        .then((v) => console.log(v))
-        .catch((e: Error) => console.error(e)); // catchがないとUnhandledPromiseRejectionWarningが出る
-    console.log(x);
+function contentToString(c: Content): string {
+  if (is<Heading>(c, "heading")) {
+    return childrenToString(c.children) + "\n";
+  } else if (is<Text>(c, "text")) {
+    return c.value;
+  } else if (is<Strong>(c, "strong")) {
+    return childrenToString(c.children);
+  } else if (is<Paragraph>(c, "paragraph")) {
+    return childrenToString(c.children) + "\n";
+  } else if (is<Break>(c, "break")) {
+    return "\n";
+  } else if (is<List>(c, "list")) {
+    return childrenToString(c.children);
+  } else if (is<ListItem>(c, "listItem")) {
+    return "・" + childrenToString(c.children);
+  } else if (is<ThematicBreak>(c, "thematicBreak")) {
+    return "\n";
+  } else if (is<Link>(c, "link")) {
+    return childrenToString(c.children) + "\n" + c.url;
+  } else {
+    console.log(`unexpected type: ${c}`);
+  }
+  return "";
 }
-
-main();
